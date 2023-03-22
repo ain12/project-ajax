c44f722dc55e95137124b795255d95255478d581
ainhoa <ainhoaactic@gmail.com>
2023-03-22 19:23:14 +0100

    create fetch and route

--- public/javascripts/staff-data.js
+++ public/javascripts/staff-data.js
@@ -1,7 +1,8 @@
 const tableBody = document.getElementById("tableBody");
+const staffForm = document.getElementById("staffForm");
 
 const getActors = async () => {
-  const response = await fetch("/api/staff");
+  const response = await fetch("/api/staff/get");
   const data = await response.json();
   data.forEach((staff) => {
     /* const actor = document.createElement("p");
@@ -18,5 +19,51 @@ const getActors = async () => {
   </tr>`;
   });
 };
+
+staffForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("funciona");
+  const firstName = document.getElementById("firstname").value;
+  const lastName = document.getElementById("lastname").value;
+  const address = document.getElementById("address").value;
+  const email = document.getElementById("emailaddress").value;
+  const store = document.getElementById("store").value;
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  const data = {
+    first_name: firstName,
+    last_name: lastName,
+    address: address,
+    email: email,
+    store_id: Number(store),
+    username: username,
+    password: password,
+  };
+
+  fetch("/api/staff/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      staffForm.innerHTML += `
+      <div class="alert alert-success" role="alert">
+        Staff added succesfully!
+    </div>
+      `;
+      // Hacer algo con la respuesta del servidor si es necesario
+    })
+    .catch((error) => {
+      //console.error("Error:" + error);
+    });
+
+  staffForm.reset();
+});
 
 getActors();
--- routes/staff.js
+++ routes/staff.js
@@ -1,7 +1,8 @@
 const { Pool } = require("pg");
 const express = require("express");
 const router = express.Router();
-
+//const fs = require("fs");
+//const pool = new Pool(fs.readFile(__dirname + '/db_credentials.json'))
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
@@ -18,7 +19,7 @@ pool.connect((error) => {
   }
 });
 
-router.get("/staff", async (req, res) => {
+router.get("/staff/get", async (req, res) => {
   pool.query(
     "SELECT staff_id, first_name, last_name, a.address, email FROM staff s inner join address a on s.address_id = a.address_id",
     (error, results) => {
@@ -28,6 +29,51 @@ router.get("/staff", async (req, res) => {
       } else {
         console.log(results);
         res.json(results.rows);
+      }
+    }
+  );
+});
+
+router.post("/staff/create", async (req, res) => {
+  const {
+    first_name,
+    last_name,
+    address,
+    email,
+    store_id,
+    username,
+    password,
+  } = req.body;
+  pool.query(
+    "INSERT INTO address (address, address2, district, city_id, postal_code, phone) VALUES ($1, $2, $3, $4, $5, $6) RETURNING address_id",
+    [address, "prueba", "N/A", 1, "prueba", "783748"],
+    (error, results) => {
+      if (error) {
+        console.error("Error creating address:", error);
+        res.status(500).send("Error creating address");
+      } else {
+        const address_id = results.rows[0].address_id;
+        pool.query(
+          "INSERT INTO staff (first_name, last_name, address_id, email, store_id, username, password) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING staff_id",
+          [
+            first_name,
+            last_name,
+            address_id,
+            email,
+            store_id,
+            username,
+            password,
+          ],
+          (error, results) => {
+            if (error) {
+              console.error("Error creating staff:", error);
+              res.status(500).send("Error creating staff");
+            } else {
+              console.log(results);
+              res.json({ staff_id: results.rows[0].staff_id });
+            }
+          }
+        );
       }
     }
   );
