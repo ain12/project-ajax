877fbdd68bb8353ea2ef2beef27e44a2ceaf847a
ainhoa <ainhoaactic@gmail.com>
2023-03-28 11:58:22 +0200

    bug localhost:3000 resolved

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const { films } = require('./films');
+const { films } = require("./films");
 const app = express();
 
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
-
-app.get('/films', async (req, res) => {
-  try {
-    const resultados = await pool.query('SELECT * FROM film');
-    res.send(resultados.rows);
-  } catch (err) {
-    console.error('Error en la consulta', err);
-    res.status(500).send('Error en la consulta');
-  }
-});
-
-// Iniciar el servidor web
-app.listen(3000, () => {
-  console.log('Servidor web iniciado en http://localhost:3000');
-});
