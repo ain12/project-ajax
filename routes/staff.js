1fd60bbd238eb1477bc4444c1811b886dfff6044
ainhoa <ainhoaactic@gmail.com>
2023-04-02 20:00:06 +0200

    typos

--- routes/staff.js
+++ routes/staff.js
@@ -34,17 +34,6 @@ router.get("/staff/get/:id", async (req, res) => {
   );
 });
 
-/* router.get("/shop/get", async (req, res) => {
-  db.query("SELECT * FROM store", (error, results) => {
-    if (error) {
-      console.error("Error fetching shops:", error);
-      res.status(500).send("Error fetching shops");
-    } else {
-      res.json(results.rows);
-    }
-  });
-}); */
-
 router.get("/shop/get", async (req, res) => {
   db.query(
     "SELECT store_id, a.address_id, a.address FROM store s inner join address a on s.address_id = a.address_id",
@@ -59,72 +48,58 @@ router.get("/shop/get", async (req, res) => {
   );
 });
 
-router.post(
-  "/staff/create",
-  async (req, res) => {
-    const {
+router.post("/staff/create", async (req, res) => {
+  const {
+    first_name,
+    last_name,
+    address,
+    email,
+    store_id,
+    active,
+    username,
+    password,
+  } = req.body;
+
+  let address_id;
+
+  const addressQuery = await db.query(
+    "SELECT address_id FROM address WHERE address = $1",
+    [address]
+  );
+
+  if (addressQuery.rows.length > 0) {
+    address_id = addressQuery.rows[0].address_id;
+  } else {
+    const newAddressQuery = await db.query(
+      "INSERT INTO address (address, address2, district, city_id, postal_code, phone) VALUES ($1, $2, $3, $4, $5, $6) RETURNING address_id",
+      [address, "prueba", "N/A", 1, "prueba", "783748"]
+    );
+    address_id = newAddressQuery.rows[0].address_id;
+  }
+
+  db.query(
+    "INSERT INTO staff (first_name, last_name, address_id, active, email, store_id, username, password) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING staff_id",
+    [
       first_name,
       last_name,
-      address,
+      address_id,
+      active,
       email,
       store_id,
-      active,
       username,
       password,
-    } = req.body;
-
-    let address_id;
-
-    const addressQuery = await db.query(
-      "SELECT address_id FROM address WHERE address = $1",
-      [address]
-    );
-
-    if (addressQuery.rows.length > 0) {
-      address_id = addressQuery.rows[0].address_id;
-    } else {
-      const newAddressQuery = await db.query(
-        "INSERT INTO address (address, address2, district, city_id, postal_code, phone) VALUES ($1, $2, $3, $4, $5, $6) RETURNING address_id",
-        [address, "prueba", "N/A", 1, "prueba", "783748"]
-      );
-      address_id = newAddressQuery.rows[0].address_id;
-    }
-
-    /* db.query(
-    "INSERT INTO address (address, address2, district, city_id, postal_code, phone) VALUES ($1, $2, $3, $4, $5, $6) RETURNING address_id",
-    [address, "prueba", "N/A", 1, "prueba", "783748"],
+    ],
     (error, results) => {
       if (error) {
-        console.error("Error creating address:", error);
-        res.status(500).send("Error creating address");
-      } else { */
-    /*  const address_id = results.rows[0].address_id; */
-    db.query(
-      "INSERT INTO staff (first_name, last_name, address_id, active, email, store_id, username, password) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING staff_id",
-      [
-        first_name,
-        last_name,
-        address_id,
-        active,
-        email,
-        store_id,
-        username,
-        password,
-      ],
-      (error, results) => {
-        if (error) {
-          console.error("Error creating staff:", error);
-          res.status(500).send("Error creating staff");
-        } else {
-          console.log(results);
-          res.json({ staff_id: results.rows[0].staff_id });
-        }
+        console.error("Error creating staff:", error);
+        res.status(500).send("Error creating staff");
+      } else {
+        console.log(results);
+        res.json({ staff_id: results.rows[0].staff_id });
       }
-    );
-  }
-  //}
-);
-//});
+    }
+  );
+});
 
 router.put("/staff/update/:id", async (req, res) => {
   const id = req.params.id;
