2906e18720d78e08f7a8b6a0bb584266aa5df897
Daniel Santamaria <daniel.santamaria@ibadia.cat>
2023-04-11 17:28:04 +0200

    films rutas pero no fetch ni como llegar a ellas

--- public/javascripts/film-data.js
+++ public/javascripts/film-data.js
@@ -4,6 +4,10 @@ const getFilms = async () => {
   const response = await fetch("/api/film/get");
   const data = await response.json();
   data.forEach((film) => {
+    console.log(film.title);
+    console.log(film.description);
+    console.log(film.release_year);
+    console.log(film.length);
     tableBody.innerHTML += 
     `<tr>
     <td>${film.title}</td>
@@ -11,10 +15,40 @@ const getFilms = async () => {
     <td>${film.release_year}</td>
     <td>${film.length}</td>
     <td>${film.rating}</td>
+    <td>${film.replacement_cost}</td>
     <td><button class="btn btn-secondary">Edit</button></td>
     <td><button class="btn btn-danger">Remove</button></td>
   </tr>`;
   });
 };
+
+// const data = {
+//   title: title,
+//   description: description,
+//   release_year: release_year,
+//   length: length,
+//   rating: rating,
+//   replacement_cost: replacement_cost,
+// }
+
+// fetch("/api/film/create", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify(data),
+// })
+// .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data);
+//       getFilms();
+//       successAlert.classList.remove("successLabel");
+//       setTimeout(() => {
+//         successAlert.classList.add("successLabel");
+//       }, 3000);
+//     })
+//     .catch((error) => {
+//       console.error("Error:" + error);
+//     });
 
 getFilms();
--- routes/film.js
+++ routes/film.js
@@ -13,7 +13,7 @@ db.connect((err, client, done) => {
 
 router.get("/film/get", async (req, res) => {
   db.query(
-    "SELECT title, description, release_year, length, rating FROM film",(error, results) => {
+    "SELECT title, description, release_year, length, rating, replacement_cost FROM film LIMIT 25",(error, results) => {
       if (error) {
         console.error("Error fetching film:", error);
         res.status(400).send(error);
@@ -25,4 +25,71 @@ router.get("/film/get", async (req, res) => {
   );
 });
 
+//get id
+router.get("/film/get/:id", async (req, res) => {
+  const id = req.params.id;
+  db.query(
+    "SELECT film_id, title, description, release_year, length, rating, replacement_cost FROM film WHERE film_id = $1",
+    [id],
+    (error, results) => {
+      if (error) {
+        console.error("Error fetching film:", error);
+        res.status(500).send("Error fetching film");
+      } else {
+        res.json(results.rows[0]);
+      }
+    }
+  );
+});
+
+
+//delete
+router.delete("/staff/delete/:id",async (req, res) =>{
+  const id = req.params.id;
+  console.log(id);
+  db.query("DELETE FROM film WHERE film_id= $1",[id],(error,results) => {
+    if (error) {
+      console.error("Error deleting film: ", error)
+      res.status(500).json({ error: "Error deleting film"});
+    } else res.json({status: "success"})
+  })
+})
+
 module.exports = router;
+
+//update
+router.put("/film/update/:id", async (req, res) => {
+  const id = req.params.id;
+  const { title, description, release_year, length, rating, replacement_cost } = req.body;
+
+  db.query(
+    "UPDATE film SET title = $1, description = $2, release_year = $3, length = $4, rating = $5, replacement_cost = $6 WHERE film_id = $7",
+    [title, description, release_year, length, rating, replacement_cost, id],
+    (error, results) => {
+      if (error) {
+        console.error("Error updating film:", error);
+        res.status(500).send("Error updating film");
+      } else {
+        res.json({ status: "success" });
+      }
+    }
+  );
+});
+
+//insert 
+router.post("/film/create", async (req, res) => {
+  const { film_id, title, description, release_year, length, rating, replacement_cost } = req.body;
+  db.query(
+    "INSERT INTO film (film_id, title, description, release_year, length, rating, replacement_cost) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING film_id",
+    [film_id, title, description, release_year, length, rating, replacement_cost],
+    (error, results) => {
+      if (error) {
+        console.error("Error creating film:", error);
+        res.status(500).send("Error creating film");
+      } else {
+        console.log(results);
+        res.json({ film_id: results.rows[0].film_id });
+      }
+    }
+  );
+});
